651ba76efbe8b3bc45041205b5c667a4f8a3337a
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-03-31 17:13:17 +0100

    calculate sum of multiples of 3 and 5

--- hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello world")
-print("My name is Mogbeyi Emmanuel Wenyinmi")
-print("Testing one two three")
---
+++ problem1.py
@@ -0,0 +1,7 @@
+def sum_of_multiples(n):
+	return sum([i for i in range(n) if i % 3 == 0 or i % 5 == 0])
+
+def test():
+	print(sum_of_multiples(10))
+
+test()
