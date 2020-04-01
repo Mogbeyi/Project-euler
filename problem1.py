46188776a29a6c7c913839b4704631c5bc0f1cfb
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-04-01 09:51:56 +0100

    Remove print statements

--- problem1.py
+++ problem1.py
@@ -1,12 +1,7 @@
-import unittest
-
 def sum_of_multiples(n):
 	return sum([i for i in range(n) if i % 3 == 0 or i % 5 == 0])
 
 def test():
 	assert sum_of_multiples(10) == 23
 	
-print("Hello world")
-print("Hello again")
-
 test()
